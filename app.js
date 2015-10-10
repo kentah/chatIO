1f4fbac21d34ebfff935e50d7959bf2331356a61
kentah <kenthow@gmail.com>
2015-10-10 14:07:44 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+
+server.listen(process.env.PORT || 3000);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection',function(socket){
+  // Send message event
+  socket.on('send message',function(data){
+    io.sockets.emit('new message',{msg: data});
+  });
+});
